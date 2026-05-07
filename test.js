548765d266a0a089379a2aa6d8ed95ce6f60fff2
jou7a <momolaruue05@gmail.com>
2026-05-07 16:07:06 +0200

    seconde f

---
+++ test.js
@@ -0,0 +1,8 @@
+function test(){
+    if(nom.value === "" || email.value === "" || message.value === ""){
+    alert("Veuillez remplir tous les champs !");
+  } elsse
+
+    alert("Formulaire envoyé avec succès !");
+
+}
